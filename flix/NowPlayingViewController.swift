09a200e4206aa8e3f34b281d1dd07bcccd582578
Nancy Gomez <nanci@Nancys-MacBook-Pro.local>
2018-02-01 23:25:24 -0800

    Managed to create basic table view set up, currently seems to have fixed height though

--- flix/NowPlayingViewController.swift
+++ flix/NowPlayingViewController.swift
@@ -8,11 +8,20 @@
 
 import UIKit
 
-class NowPlayingViewController: UIViewController {
-
+class NowPlayingViewController: UIViewController, UITableViewDataSource {
+    
+    // UI tableView
+    @IBOutlet weak var tableView: UITableView!
+    
+    
+    // default func
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        // We will provide the data for the table view
+        tableView.dataSource = self
+        
+        // API REQUEST -----> {
         let url = URL(string: "https://api.themoviedb.org/3/movie/now_playing?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed")!
         
         let request = URLRequest(url: url, cachePolicy: .reloadIgnoringLocalCacheData, timeoutInterval: 10)
@@ -24,20 +33,37 @@ class NowPlayingViewController: UIViewController {
             if let error = error {
                 print(error.localizedDescription)
             } else if let data = data {
+                // receives data from url and we make it a json object
                 let dataDictionary = try! JSONSerialization.jsonObject(with: data, options: []) as! [String: Any]
+                // Now we extract the movies from the json object
                 let movies = dataDictionary["results"] as! [[String: Any]]
                 
+                // Display each movie in the console
                 for movie in movies {
                     let title = movie["title"] as! String
                     print(title)
                 }
                 
-//                print(dataDictionary)
+                // print(dataDictionary)
             }
         }
+        // Start the task to get the info!
         task.resume()
+        // } <------- END API REQUEST
     }
-
+    
+    // UITableViewDataSource required func
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10;
+    }
+    
+    // UITableViewDataSource required func
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "MovieCell", for: indexPath)
+        return cell
+    }
+    
+    // default func
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
